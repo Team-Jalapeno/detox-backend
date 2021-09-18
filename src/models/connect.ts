24ea7c7611e58432f9a5ec64fe11db6fbc2f8780
ashikka <ashikagupta28@gmail.com>
2021-09-19 00:01:28 +0530

    fix: Console log on connection

--- src/models/connect.ts
+++ src/models/connect.ts
@@ -1,10 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export default async function connectDB(URL: string) {
-    await mongoose.connect(URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
+  await mongoose
+    .connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    })
+    .then(() =>
+      console.info({
+        Message: "DB connected successfully",
+        error: "Value: null",
+      })
+    )
+    .catch((e) => {
+      console.error({ Message: "DB connection failed", error: e.toString() });
+      process.exit(1);
     });
 }

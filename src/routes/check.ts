98824e25b6f93ba522eaa519e71f15015d409813
thebongy <rishit.bansal0@gmail.com>
2021-09-18 18:35:56 +0530

    feat: Text scanning route completed

--- src/routes/check.ts
+++ src/routes/check.ts
@@ -6,7 +6,6 @@ const router = express.Router();
 const DISCOVERY_URL = 'https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1';
 
 router.post("/text", async (req, res, next) => {
-    console.log(req.body);
     const textData = req.body.text.toString();
 
     const client = (await google.discoverAPI(DISCOVERY_URL)) as any;
@@ -17,15 +16,30 @@ router.post("/text", async (req, res, next) => {
         },
         requestedAttributes: {
             TOXICITY: {},
+            IDENTITY_ATTACK: {},
+            INSULT: {},
+            PROFANITY: {},
+            THREAT: {},
+            SEXUALLY_EXPLICIT: {},
+            FLIRTATION: {},
         },
     };
 
-    console.log(client.comments.analyze(
+    const resp = (await client.comments.analyze(
         {
             key: process.env.PERSPECTIVE_API_KEY,
             resource: analyzeRequest,
-        }));
-    res.send("OK");
+        })).data;
+    const scores: {[type: string]: any} = {};
+
+    for (const attribute in resp['attributeScores']) {
+        scores[attribute] = resp['attributeScores'][attribute]['summaryScore']['value'];
+    }
+
+    res.json({
+        success: true,
+        data: scores,
+    });
 });
 
 export default router;

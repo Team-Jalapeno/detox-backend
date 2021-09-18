bc147242ba06118de66cc0bc30cac6c1da363940
thebongy <rishit.bansal0@gmail.com>
2021-09-18 20:56:52 +0530

    feat: Base routesfor reporting

--- src/routes/community.ts
+++ src/routes/community.ts
@@ -2,8 +2,20 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    res.send("Pong!");
+router.get("/report", (req, res, next) => {
+    const url = req.body.url;
+    // get all reports for a page
+});
+
+router.post("/report", (req, res, next) => {
+    const url = req.body.url;
+    const vote = parseFloat(req.body.vote);
+    const selector = req.body.selector.toString();
+    const userId = req.body.userId.toString();
+
+    // Create a new report if doesnt exist
+    // If exists, make sure that user hasnt already voted
+    // Update score based on formula (for now mean)
 });
 
 export default router;

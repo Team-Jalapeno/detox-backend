80fb8a08c0bcc2129cb3b4612578f92742bd9f2f
thebongy <rishit.bansal0@gmail.com>
2021-09-19 11:23:48 +0530

    fix: allow cors

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import routes from './routes';
 import connectDB from './models/connect';
@@ -10,6 +11,8 @@ connectDB(process.env.MONGO_URL);
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.urlencoded({
     extended: false,
